243255c6a4536c9ddf3967b6cdc80af005bb4bc8
Ricky Witherspoon <richardwitherspoon3@gmail.com>
2026-02-01 15:05:48 -0500

    Update spm-audit to 0.3.0

--- Formula/spm-audit.rb
+++ Formula/spm-audit.rb
@@ -1,8 +1,8 @@
 class SpmAudit < Formula
   desc "Audit and update Swift Package Manager dependencies"
   homepage "https://github.com/Rspoon3/spm-audit"
-  url "https://github.com/Rspoon3/spm-audit/archive/refs/tags/0.2.0.tar.gz"
-  sha256 "b0717480af26188a6b920b82fc7cada6b47751a507797e4bb4ed668eae8cb35c"
+  url "https://github.com/Rspoon3/spm-audit/archive/refs/tags/0.3.0.tar.gz"
+  sha256 "f426be0a26b67e2260b49f93d17d70b9ce8705dfc1ccdb4e4a819e1f5fde9fdd"
   license "MIT"
 
   depends_on xcode: ["14.0", :build]
